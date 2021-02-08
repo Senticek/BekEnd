5815e60e7ca8d734a6523d4e424e045cf503521a
Tomas <tomas.svoboda111@gmail.com>
2021-02-08 18:17:33 +0100

    Pridani modelu databaze

---
+++ app/Model/Database.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Model;
+
+use Nette;
+
+class ArticleManager
+{
+	use Nette\SmartObject;
+
+	private Nette\Database\Explorer $database;
+
+	public function __construct(Nette\Database\Explorer $database)
+	{
+		$this->database = $database;
+	}
+}
+
+?>
