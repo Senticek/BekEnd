adc2044093c0cee476a381d4d037a15d8dab85bd
Tomas <tomas.svoboda111@gmail.com>
2021-02-10 16:20:28 +0100

    Pridani Register formu

--- app/Module/LoginModule/Presenters/RegInPresenter.php
+++ app/Module/LoginModule/Presenters/RegInPresenter.php
@@ -1,13 +1,33 @@
 <?php
-namespace App,Presenters;
+namespace App\Presenters;
 
 
 use Nette;
 use Nette\Application\UI\Form;
-
+use Nette\Utils\Html;
+use Tracy\Debugger;
+use Tracy\Dumper;
 
 class RegPresenter extends Nette\Application\UI\Presenter
 {
+    protected function createComponentRegInForm(): Form
+	{
+    $form->addText('username', 'Your username:')
+	->setRequired('Enter your username');
+
+
+    $form->addPassword('password', 'Choose password:')
+	->setRequired('Choose your password')
+	->addRule($form::MIN_LENGTH, 'The password is too short: it must be at least %d characters', 3);
+
+   $form->addPassword('password2', 'Reenter password:')
+	->setRequired('Reenter your password')
+	->addRule($form::EQUAL, 'Passwords do not match', $form['password']);
+
+    $form->addEmail('email', 'email')
+	->setRequired('enter email')
+	->addRule($form::EMAIL);
+    }
 
 }
 ?>
