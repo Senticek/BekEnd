614a4b83afb322497854dfea72bcf2bbe06f6a88
Tomas <tomas.svoboda111@gmail.com>
2021-02-21 17:30:46 +0100

    MistakeDelete

---
+++ app/Model/FormValidate.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Model;
+use Nette;
+use Nette\Security\SimpleIdentity;
+use Nette\Database\Context;
+use Nette\Database\Explorer;
+use Nette\Security\User;
+use Nette\Utils\Validators;
+
+class FormValidate
+{
+
+
+	
+
+	public static function validateEmail(Control $control): bool
+	{
+		return Validators::isEmail((string) $control->getValue());
+	}
+
+
+
+}
+?>
