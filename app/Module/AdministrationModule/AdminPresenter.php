cadd07de1346ccdccea148cd4735dc2a7c17ba12
Tomas <tomas.svoboda111@gmail.com>
2021-02-21 16:40:05 +0100

    RemodelingTree And Making AdminPagePresenter

---
+++ app/Module/AdministrationModule/AdminPresenter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Presenters;
+
+use App\Presenters\HomepagePresenter;
+use Nette;
+use Nette\Application\UI\Presenter;
+use App\Model\Database;
+use Nette\Application\UI\Form;
+
+class AdminPresenter extends Presenter
+{
+
+
+
+
+
+}
+?>
