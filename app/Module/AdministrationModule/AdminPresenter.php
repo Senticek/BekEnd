b167e0e04938b5ec9663e241ad36aadf9da536a8
Tomas <tomas.svoboda111@gmail.com>
2021-02-21 17:26:55 +0100

    Presenter Admin Role verification

--- app/Module/AdministrationModule/AdminPresenter.php
+++ app/Module/AdministrationModule/AdminPresenter.php
@@ -11,6 +11,47 @@ use Nette\Application\UI\Form;
 class AdminPresenter extends Presenter
 {
 
+    private Database $dtb_data;
+   
+    private Template $post;
+    public function __construct(Database $dtb_data)
+    {
+     $this->dtb_data =$dtb_data ;
+    
+     
+       //this forces all js and css to reload every single time version changes
+       
+       
+    }
+    
+    public function renderDefault(): void
+	{
+        if($this->user->isInRole('admin')){
+		$this->template->posts = $this->dtb_data
+    ->getPublicArticles();
+   
+
+    $this->template->headTitle = $this->template->posts->get(1);
+    $this->template->subtitle = $this->template->posts->get(2);
+    $this->template->aboutUS = $this->template->posts->get(3);
+    $this->template->logo = $this->template->posts->get(4);
+
+    $this->template->adresses = $this->dtb_data
+    ->getAdresses();
+    $this->template->portfolio = $this->dtb_data->getPortfolioItems();
+    $this->template->postsA = $this->template->portfolio->get(100);
+    $this->template->links = $this->dtb_data
+    ->getLinks();
+    $this->template->socials = $this->dtb_data->getSoc();}else{$this->redirect(':Homepage:');}
+    
+  /*  parent::startup();
+	if ($this->getUser()->isAllowed('edit')) {
+		$this->template->headTitle = "ahojky";
+	}*/
+
+   
+
+	}
 
 
 
